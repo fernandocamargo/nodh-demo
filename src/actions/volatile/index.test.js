e49e058d287c04b1108e98d919acd966b6e66a97
Fernando Camargo <fernando.camargo@sellics.com>
2019-05-17 06:27:39 +0200

    Testing...

--- src/actions/volatile/index.test.js
+++ src/actions/volatile/index.test.js
@@ -4,22 +4,33 @@ import { mount, unmount } from "mutations";
 
 import actions from ".";
 
-const save = jest.fn().mockImplementation(md5);
+const getMock = () => {
+  const save = jest.fn().mockImplementation(md5);
+  const mock = { volatile: { save } };
 
-const mock = { volatile: { save } };
+  return { save, mock };
+};
 
-test("Expect action to save into volatile layer using mount mutation", () => {
+test(`
+  Expect mount action to save into volatile layer using mount mutation.
+`, () => {
+  const { save, mock } = getMock();
   const action = actions.mount(mock);
   const output = action();
   const expected = md5(mount());
 
+  expect(save).toHaveBeenCalled();
   expect(output).toBe(expected);
 });
 
-test("Expect action to save into volatile layer using unmount mutation", () => {
+test(`
+  Expect unmount action to save into volatile layer using unmount mutation.
+`, () => {
+  const { save, mock } = getMock();
   const action = actions.unmount(mock);
   const output = action();
   const expected = md5(unmount());
 
+  expect(save).toHaveBeenCalled();
   expect(output).toBe(expected);
 });
